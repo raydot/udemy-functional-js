4a3f4f0eae5a06c0e398efe2eeed6b49f906ff4d
Dave Kanter <moatgoat@hotmail.com>
2021-06-24 13:42:31 -0700

    Initial commit

---
+++ ex_01.js
@@ -0,0 +1,26 @@
+var currentUser = 0,
+  users = [
+    { name: "James", score: 30, tries: 1 },
+    { name: "Mary", score: 110, tries: 4 },
+    { name: "Henry", score: 80, tries: 3 },
+  ];
+
+var updateScore = function (newAmt) {
+  users[currentUser].score += newAmt;
+};
+
+var returnUsers = function () {
+  return users;
+};
+
+var updateTries = function () {
+  users[currentUser].trues++;
+};
+
+var updateUser = function (newUser) {
+  currentUser = newUser;
+};
+
+console.log(returnUsers());
+updateScore(20);
+console.log(returnUsers());
