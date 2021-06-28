09806adc01a9f8c803d9a3935fc1eaa01f7b6e04
Dave Kanter <moatgoat@hotmail.com>
2021-06-28 10:32:51 -0700

    Added comments from instructor on how to complete exercise.

--- ex_01.js
+++ ex_01.js
@@ -7,18 +7,22 @@ var currentUser = 0,
 
 var userStack = [users];
 
+// Not pure, has side effects.
 var updateScore = function (newAmt) {
   users[currentUser].score += newAmt;
 };
 
+// Not necessary now what we're not using namespace pattern
 var returnUsers = function () {
   return users;
 };
 
+// Not pure
 var updateTries = function () {
   users[currentUser].trues++;
 };
 
+// Not pure
 var updateUser = function (newUser) {
   currentUser = newUser;
 };
@@ -26,3 +30,10 @@ var updateUser = function (newUser) {
 console.log(returnUsers());
 updateScore(20);
 console.log(returnUsers());
+
+/**
+ * This of a function as receiving data and returning data.
+ * What happens inside that function only acts on what is received,
+ * Does not modify anything outside the function.
+ * May need a function that updates the array.
+ */
