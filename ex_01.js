9c40c1e3af8d9b3af29e0043c90375866683cc6c
Dave Kanter <dave@lastrev.com>
2021-06-28 13:36:55 -0700

    Trying to work with named parameters.

--- ex_01.js
+++ ex_01.js
@@ -7,19 +7,49 @@ var currentUser = 0,
 
 var userStack = [users];
 
+// This is going to be the one impure function:
+const updateUsers = (name, score = 0, tries = 0) => {
+  //console.log("rest:", rest);
+  //console.log("tos", typeof score, "tot", typeof tries);
+  console.log(score);
+  console.log(tries);
+  if (!score === 0) {
+    console.log("Score:");
+    console.log(getKeyByValue(users, name));
+  }
+  if (!tries === 0) {
+    console.log("Tries:");
+    console.log(getKeyByValue(users, name));
+  }
+};
+
+// FROM https://stackoverflow.com/questions/9907419/how-to-get-a-key-in-a-javascript-object-by-its-value
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
+
+/**
+ * USAGE
+ *   const map = {"first" : "1", "second" : "2"};
+ 
+*  console.log(getKeyByValue(map,"2"));
+ */
+
 // Not pure, has side effects.
-var updateScore = function (newAmt) {
-  users[currentUser].score += newAmt;
+var updateScore = function (name, newAmt) {
+  // users[currentUser].score += newAmt;
+  return users[name].score + newAmt;
 };
 
 // Not necessary now what we're not using namespace pattern
-var returnUsers = function () {
-  return users;
-};
+// var returnUsers = function () {
+//   return users;
+// };
 
 // Not pure
 var updateTries = function () {
-  users[currentUser].trues++;
+  return users[currentUser].tries + 1;
+  // users[currentUser].trues++;
 };
 
 // Not pure
@@ -27,13 +57,18 @@ var updateUser = function (newUser) {
   currentUser = newUser;
 };
 
-console.log(returnUsers());
-updateScore(20);
-console.log(returnUsers());
+// console.log(returnUsers());
+// updateScore(20);
+// console.log(returnUsers());
 
 /**
+ * INSTRUCTOR NOTES:
  * This of a function as receiving data and returning data.
  * What happens inside that function only acts on what is received,
  * Does not modify anything outside the function.
  * May need a function that updates the array.
  */
+
+// TESTS
+updateUsers("James", { score: 20 });
+updateUsers("Henry", { tries: 1 });
